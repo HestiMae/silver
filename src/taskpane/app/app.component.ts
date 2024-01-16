fb87eda540a63db2e5b67b9ace45cc732cb4435f
HestiMae <hestiaheartstrings@gmail.com>
2024-01-16 15:35:36 +1100

    Fixed EsLint Bugs. CSV Merger using SheetJS Eslint bugs on webstorm took way too long. Merging is very fast

--- src/taskpane/app/app.component.ts
+++ src/taskpane/app/app.component.ts
@@ -3,21 +3,40 @@ import { Component } from "@angular/core";
 import "../../../assets/icon-16.png";
 import "../../../assets/icon-32.png";
 import "../../../assets/icon-80.png";
+
 const template = require("./app.component.html");
+import fileDialog from "file-dialog";
+import XLSX, { WorkBook } from "xlsx";
 /* global console, Excel, require */
 
 @Component({
   selector: "app-home",
-  template,
+  template
 })
 export default class AppComponent {
   welcomeMessage = "Welcome";
 
   async run() {
     try {
+      /**
+       * Sheetjs here
+       */
+      fileDialog({multiple:true}).then(files => {
+        const wb = XLSX.utils.book_new();
+        Promise.all(Array.prototype.map.call(files, (file) => file.text())).then((file_texts) => {
+          file_texts.forEach(((text, i) => {
+            let tempWorkbook = XLSX.read(text, { type: "binary" });
+            XLSX.utils.book_append_sheet(wb, tempWorkbook.Sheets[tempWorkbook.SheetNames[0]], files[i].name.substring(0, 30).split('.')[0])
+
+          }))
+          XLSX.writeFile(wb, "merged.xlsx");
+        })
+      });
+
+
       await Excel.run(async (context) => {
         /**
-         * Insert your Excel code here
+         * Excel API here
          */
         const range = context.workbook.getSelectedRange();
 
@@ -25,7 +44,7 @@ export default class AppComponent {
         range.load("address");
 
         // Update the fill color
-        range.format.fill.color = "yellow";
+        // range.format.fill.color = "yellow";
 
         await context.sync();
         console.log(`The range address was ${range.address}.`);
